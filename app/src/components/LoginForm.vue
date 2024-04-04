24016b6956a36176273f1b26ce6a981f7ac81fc7
Bodoz <mattiabodo05@gmail.com>
2024-04-04 09:55:25 +0200

    login form

--- app/src/components/LoginForm.vue
+++ app/src/components/LoginForm.vue
@@ -1,13 +1,54 @@
 <template>
     <v-dialog
         max-width="600"
-        persistent
         :model="dialog"
+        persistent
     >
       <v-card
           prepend-icon="mdi-account"
-          title="User Profile"
+          title="Login"
       >
+        <v-card-text>
+          <v-form v-model="valid">
+            <v-container>
+              <v-row>
+                <v-col
+                    cols="12"
+                    md="4"
+                >
+                  <v-text-field
+                      v-model="username"
+                      :counter="32"
+                      :rules="usernameRules"
+                      label="username"
+                      required
+                  ></v-text-field>
+                </v-col>
+
+                <v-col
+                    cols="12"
+                    md="4"
+                >
+                  <v-text-field
+                      v-model="password"
+                      :counter="16"
+                      :rules="passwordRules"
+                      label="password"
+                      required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-card-text>
+        <v-card-actions class="px-10 pb-6">
+          <v-spacer></v-spacer>
+          <v-btn type="submit"
+                 color="primary"
+                 variant="elevated"
+                 class="px-4"
+          >Login</v-btn>
+        </v-card-actions>
       </v-card>
     </v-dialog>
 </template>
@@ -15,10 +56,25 @@
 <script>
 export default {
   props: {
-    show: Boolean,
+    dialog: Boolean,
   },
+  //dialog: false,
   data: () => ({
-    //dialog: false,
+    valid: false,
+    username: '',
+    password: '',
+    usernameRules: [
+      value => {
+        if (value) return true
+        else return 'username is required.'
+      },
+    ],
+    passwordRules: [
+      value => {
+        if (value && value?.trim().length > 0) return true
+        else return 'password is required too.'
+      },
+    ],
   }),
   computed: {
     dialog() {
