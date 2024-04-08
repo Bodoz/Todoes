af206e6ae7b325a99c388369245e1d31baf74391
Bodoz <mattiabodo05@gmail.com>
2024-04-08 07:56:45 +0200

    new commit

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,33 +1,35 @@
 <template>
   <v-app>
-    <v-app-bar title="This is my todoes list">
+    <v-app-bar title="Todo List">
       <v-tabs>
         <v-tab
-          color="indigo"
-          v-for="link in links"
-          :to="link.to"
+            color="indigo"
+            v-for="link in links"
+            :to="link.to"
         >{{ link.text }}</v-tab>
       </v-tabs>
     </v-app-bar>
-      <v-main>
-        <v-container>
-          <v-sheet class="ma-2 pa-2">
-            <RouterView />
-          </v-sheet>
-        </v-container>
-      </v-main>
-    <login-form
-      :show="show_login"
-    ></login-form>
+
+    <v-main>
+      <v-container>
+        <v-sheet class="ma-2 pa-2">
+          <RouterView />
+
+        </v-sheet>
+      </v-container>
+    </v-main>
+
+    <LoginForm
+        :show="show_login"
+    >
+    </LoginForm>
   </v-app>
 </template>
 
 <script>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import LoginForm from './components/LoginForm.vue'
 
-export default{
+export default {
   components: { LoginForm },
   data() {
     return {
--- app/src/components/LoginForm.vue
+++ app/src/components/LoginForm.vue
@@ -1,85 +1,85 @@
 <template>
+  <div class="pa-4 text-center">
     <v-dialog
         max-width="600"
-        :model="dialog"
         persistent
+        v-model="dialog"
     >
       <v-card
           prepend-icon="mdi-account"
           title="Login"
       >
-        <v-card-text>
-          <v-form v-model="valid">
-            <v-container>
-              <v-row>
-                <v-col
-                    cols="12"
-                    md="4"
-                >
-                  <v-text-field
-                      v-model="username"
-                      :counter="32"
-                      :rules="usernameRules"
-                      label="username"
-                      required
-                  ></v-text-field>
-                </v-col>
+        <v-form v-model="valid">
+          <v-container>
+            <v-row>
+              <v-col
+                  cols="12"
+                  md="6"
+              >
+                <v-text-field
+                    v-model="username"
+                    :counter="32"
+                    maxlength="32"
+                    :rules="usernameRules"
+                    label="Username"
+                    required
+                ></v-text-field>
+              </v-col>
 
-                <v-col
-                    cols="12"
-                    md="4"
-                >
-                  <v-text-field
-                      v-model="password"
-                      :counter="16"
-                      :rules="passwordRules"
-                      label="password"
-                      required
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-form>
-        </v-card-text>
-        <v-card-actions class="px-10 pb-6">
+              <v-col
+                  cols="12"
+                  md="6"
+              >
+                <v-text-field
+                    v-model="password"
+                    :counter="16"
+                    maxlength="16"
+                    :rules="passwordRules"
+                    label="Password"
+                    required
+                ></v-text-field>
+                <!-- il max length non serve nel login quanto nel signup -->
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+        <v-card-actions class="px-4 pb-5">
           <v-spacer></v-spacer>
-          <v-btn type="submit"
-                 color="primary"
-                 variant="elevated"
-                 class="px-4"
+          <v-btn
+              type="submit"
+              color="primary"
           >Login</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
+  </div>
 </template>
-
 <script>
 export default {
   props: {
-    dialog: Boolean,
+    show: Boolean,
   },
-  //dialog: false,
   data: () => ({
     valid: false,
     username: '',
     password: '',
     usernameRules: [
-      value => {
-        if (value) return true
-        else return 'username is required.'
+      (value) => {
+        if (value && value?.trim().length > 0) return true // anche senza value
+        else return 'Username is required.'                // per il value? che sostituisce
       },
     ],
-    passwordRules: [
-      value => {
+    passwordRules : [
+      (value) => {
         if (value && value?.trim().length > 0) return true
-        else return 'password is required too.'
+        else return 'Password is required.'
       },
     ],
   }),
   computed: {
     dialog() {
       return this.show
-    },
-  },
+    }
+  }
 }
 </script>
